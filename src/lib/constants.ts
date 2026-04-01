01b6e69496783f63dd4ef3474a8a73ad7d03f9be
FrancisM68 <adolfo_martins@hotmail.com>
2026-04-01 18:33:39 +0100

    feat: add TERMINAL_STATUSES, OTHER species label, disposalMethodLabel

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,13 @@
 export const VALID_RESCUE_SLUGS = ["dar"] as const;
 
 export type RescueSlug = (typeof VALID_RESCUE_SLUGS)[number];
+
+export const TERMINAL_STATUSES = [
+  "ADOPTED",
+  "RETURNED_TO_OWNER",
+  "EUTHANISED",
+  "DIED_IN_CARE",
+  "TNR_RETURNED",
+] as const;
+
+export type TerminalStatus = (typeof TERMINAL_STATUSES)[number];
